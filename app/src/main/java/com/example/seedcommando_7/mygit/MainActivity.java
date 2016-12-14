6d1a964ecfe19ab1407c8f58a304cfe572074ab2
mayurmanjare <mayurmanjare4232@gmail.com>
2016-12-14 14:28:01 +0530

    On click displays vikas

---
+++ app/src/main/java/com/example/seedcommando_7/mygit/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.seedcommando_7.mygit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       b=(Button)findViewById(R.id.button);
+
+    }
+    public void show1(View v)
+    {
+        Toast.makeText(this,"VIKAS",Toast.LENGTH_LONG).show();
+    }
+}
